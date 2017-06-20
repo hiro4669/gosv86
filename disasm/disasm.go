d191e3cdd3783729d530d7c80019422ac79ce166
Hiroaki Fukuda <hiroaki@java2.jp>
2017-06-20 20:28:38 +0900

    add Run, Init and fetch to disasm

--- disasm/disasm.go
+++ disasm/disasm.go
@@ -2,13 +2,38 @@ package disasm
 
 import "fmt"
 
-
 type Disasm struct {
-	pc uint16
+	pc   uint16
 	text []byte
 }
 
-func (dis Disasm) Run() {
+func (dis *Disasm) fetch() byte {
+	v := dis.text[dis.pc]
+	dis.pc++
+	return v
 
+}
+
+func (dis *Disasm) Init(text []byte) {
+	fmt.Println("init called")
+	dis.pc = 0
+	dis.text = make([]byte, len(text))
+	copy(dis.text, text)
+}
+
+func (dis *Disasm) Run() {
 	fmt.Println("start disasm")
+
+	for i := 0; i < len(dis.text); i++ {
+		fmt.Printf("%02x ", dis.fetch())
+	}
+	fmt.Printf("\npc = %d\n", dis.pc)
+
+	//	fmt.Println(dis.pc)
+	//	fmt.Println(dis.text)
+	/*
+		for _, v := range dis.text {
+			fmt.Printf("%02x ", v)
+		}
+	*/
 }
--- main.go
+++ main.go
@@ -2,12 +2,83 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"./disasm"
 )
 
-func main() {
-	var dis disasm.Disasm
-	fmt.Println("Hello Disasm")
-	dis.Run()
+func fetch2(data []byte, idx int) uint16 {
+	return uint16(data[idx+1])<<8 | uint16(data[idx])
 }
 
+func partialCopy(src []byte, off int, len int) []byte {
+	return append(make([]byte, 0), src[off:off+len]...)
+}
+
+func getText(data []byte) []byte {
+	var len uint16 = fetch2(data, 8)
+	text := partialCopy(data, 0x20, int(len))
+	return text
+}
+
+func readFile(name string) []byte {
+	file, err := os.Open(name)
+	if err != nil {
+		fmt.Println("cannot open file : " + name)
+	}
+	defer file.Close()
+
+	var fSize int
+	if fi, err := file.Stat(); err == nil {
+		if fSize = int(fi.Size()); fSize == 0 {
+			fmt.Println("size of the file is zero: exit")
+			os.Exit(1)
+		}
+	}
+	var buf []byte = make([]byte, fSize)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			break
+		}
+	}
+	return buf
+
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("引数の数が違います")
+		os.Exit(1)
+	}
+	fileName := os.Args[1]
+	fmt.Println(fileName)
+
+	buf := readFile(fileName)
+
+	for i := 0; i < len(buf); i++ {
+		if i%16 == 0 {
+			fmt.Println()
+		}
+		fmt.Printf("%02x ", buf[i])
+	}
+	fmt.Println()
+
+	text := getText(buf)
+
+	for i := 0; i < len(text); i++ {
+		if i%16 == 0 {
+			fmt.Println()
+		}
+		fmt.Printf("%02x ", text[i])
+	}
+	fmt.Println()
+
+	var dis *disasm.Disasm = new(disasm.Disasm)
+	fmt.Println("Hello Disasm")
+	dis.Init(text)
+	dis.Run()
+}
